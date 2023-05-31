e197aee0d1eae512e539b849d0c22a498863e1f9
Stepan Vlasenko <Reyden732@gmail.com>
2023-05-31 17:53:54 +0300

    initial commit

---
+++ backend/databaseModels.py
@@ -0,0 +1,38 @@
+from peewee import *
+from datetime import date
+
+db = SqliteDatabase('dev.db')
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class User(BaseModel):
+    firstName = CharField()
+    secondName = CharField()
+    middleName = CharField()
+    birthDate = DateField()
+    city = CharField()
+    school = CharField()
+    avatarURL = CharField()
+
+class Post(BaseModel):
+    userId = IntegerField()
+    title = CharField()
+    content = TextField()
+    publishDate = DateField()
+    imageURL = CharField()
+
+class Comment(BaseModel):
+    userId = IntegerField()
+    postId = IntegerField()
+    content = TextField()
+    publishDate = DateField()
+    imageURL = CharField()
+
+db.connect()
+db.create_tables(models = [User, Post, Comment])
+
+myUser = User(firstName = 'Kyle', secondName = 'Jackson', middleName = 'Bob', birthDate = date(2006, 1, 6), city = 'New-York', school = 'Monaco School', avatarURL = 'https://google.com')
+myUser.save()
+print(myUser.firstName, myUser.birthDate)
