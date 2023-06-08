dca6b999c8eb11a31c222027a1268daaefd5a12c
Stepan Vlasenko <Reyden732@gmail.com>
2023-06-08 16:11:13 +0300

    add directory type

--- frontend/assets/ts/firebase.ts
+++ frontend/assets/ts/firebase.ts
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getStorage, ref, getDownloadURL, uploadBytes } from 'firebase/storage'
 
+type directory = 'user' | 'post' | 'comment'
+
 const firebaseConfig = {
   apiKey: "AIzaSyB7m9tb6RQjz2d9EhScsapzk5PMabPqGLc",
   authDomain: "chemp-c9930.firebaseapp.com",
@@ -11,15 +13,15 @@ const firebaseConfig = {
   appId: "1:775737144466:web:52f9e5a5d1c7b01325e24b"
 };
 
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 const storage = getStorage()
 
-export const sendFile = async (directory: string, fileName: string, data: Uint8Array) => {
+export const sendFile = async (directory: directory, fileName: string, data: Uint8Array) => {
     const pathRef = ref(storage, `${directory}/${fileName}`)
     return await uploadBytes(pathRef, data)
 }
-export const getFileURL = async (directory: string, fileName: string) => {
+export const getFileURL = async (directory: directory, fileName: string) => {
     const pathRef = ref(storage, `${directory}/${fileName}`)
     return await getDownloadURL(pathRef)
 }
