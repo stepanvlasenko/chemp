ea28d35be6dec8fad345a496478c606edc9713bf
Stepan Vlasenko <Reyden732@gmail.com>
2023-06-08 18:18:50 +0300

    add avatarURL to header

--- frontend/components/TheHeader/TheHeader.tsx
+++ frontend/components/TheHeader/TheHeader.tsx
@@ -1,36 +1,38 @@
 import './TheHeader.css'
 
-export default function TheHeader(){
-    
+interface HeaderProps {
+    avatarURL?: string
+}
+
+export default function TheHeader({ avatarURL = "https://upload.wikimedia.org/wikipedia/commons/7/70/Solid_white.svg" }: HeaderProps) {
+
     return (
-        <>
         <header className="header">
-    <div className="lorem">
-        Lorem
-    </div>
-    <nav className="navigation"> 
-        <ul className="navigation_list">
-            <li> 
-                <a className="navigation_link"> 
-                    Главная     
-                </a> 
-            </li>
-            <li> 
-                <a className="navigation_link"> 
-                    Войти       
-                </a> 
-            </li>
-            <li> 
-                <a className="navigation_link"> 
-                    Регистрация 
-                </a> 
-            </li>
-        </ul>
-        <a className="navigation_profile">
-          <div className="profile_icon"></div>
-        </a>
-    </nav>
-</header>
-        </>
+            <div className="lorem">
+                Lorem
+            </div>
+            <nav className="navigation"> 
+                <ul className="navigation__list">
+                    <li> 
+                        <a className="navigation__link"> 
+                            Главная     
+                        </a> 
+                    </li>
+                    <li> 
+                        <a className="navigation__link"> 
+                            Войти       
+                        </a> 
+                    </li>
+                    <li> 
+                        <a className="navigation__link"> 
+                            Регистрация 
+                        </a> 
+                    </li>
+                </ul>
+                <a className="navigation__profile">
+                    <img className='navigation__avatar' src={avatarURL} />
+                </a>
+            </nav>
+        </header>
     )
 }
