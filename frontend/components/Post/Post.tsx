4bf6f9b941f237fd877e61e30374dde571889ee9
Stepan Vlasenko <Reyden732@gmail.com>
2023-06-11 20:18:48 +0300

    update post

--- frontend/components/Post/Post.tsx
+++ frontend/components/Post/Post.tsx
@@ -1,37 +1,32 @@
+import { useEffect, useState } from 'react'
+import { IPost, IUser } from '../../assets/ts/types'
 import './Post.css'
-
-/**
- * ToDo:
- * Move to 'types.ts'
- * request user
- */
-interface IPost {
-    id: number
-    userId: number
-    title: string
-    content: string
-    publishDate: string
-    imageURL: string
-}
+import { useAPI } from '../../assets/ts/api'
 
 interface PostProps {
     post: IPost
 }
 
 export default function Post({post}: PostProps) {
+    const [user, setUser] = useState<IUser>()
+
+    const userAPI = useAPI().useUserAPI()
+    useEffect(() => {
+        userAPI.getUser(post.userId).then((user) => setUser(user))
+    }, [])
 
     return (
         <div className='post'>
             <div className='post__info'>
                 <img className='info__avatar' src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1280px-Image_created_with_a_mobile_phone.png' />
                 <div className='info__bio'>
-                    <h1>Фамилия Имя Отчество</h1>
+                    <h1>{user?.middleName} {user?.firstName} {user?.secondName}</h1>
                     <h2>{post.publishDate}</h2>
                 </div>
             </div>
             <div className='post__content'>
                 <h1>{post.title}</h1>
-                <img className="content__image" src={post.imageURL} />
+                <img className="post-content__image" src={post.imageURL} />
             </div>
         </div>
     )
